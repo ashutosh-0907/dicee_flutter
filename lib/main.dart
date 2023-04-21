d4796149b370626ef2677338d19e177b48f712af
ashutosz <ashutoshrai08022003@gmail.com>
2023-04-21 17:07:28 +0530

    My Business Card

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
                 height: 20.0,
                 width: 150.0,
                 child: Divider(
-                  color: Colors.white,                  
+                  color: Colors.white,
                 ),
               ),
               Padding(
